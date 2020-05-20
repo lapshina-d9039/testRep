46df3d63469b559d52e6b1f0f6a15b0d43c334bf
Darya <lapshinad@student.douglascollege.ca>
2020-05-20 11:17:37 -0700

    Creating a method

--- src/AppForTest.java
+++ src/AppForTest.java
@@ -12,8 +12,34 @@ public class AppForTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
+	}
+	public long computeFibbonacci(long n) {
+		
+		// if input is negative - throw exception
+		if (n < 0) {
+			throw new IllegalArgumentException();
+		}
+		// initialize fields for result and previous number
+		long result = 1;
+		long previousFib = 1;
+	
+		// fibonacci of 0 and 1
+		if (n <= 1) {
+			return n;
+		}
+		// start loop at 2, until less than n
+		for (int i = 2; i < n; i++) {
+			// set temp to the current partial result
+			long temp = result;
+			// add the previous number to the result
+			result += previousFib;
+			// reassign the previous to the temp (step to the next number)
+			previousFib = temp;
+
+		}
+		return result;
 	}
 
 }
