50ba824ecddbd3c4d3088352681745663a8d34e1
Darya <lapshinad@student.douglascollege.ca>
2020-05-20 12:53:13 -0700

    Added recursive Method

--- src/AppForTest.java
+++ src/AppForTest.java
@@ -18,7 +18,8 @@ public class AppForTest {
 		System.out.println("Input Number to get a Fibbonacci of:");
 		long calc = input.nextLong();
 		long result = computeFibbonacci(calc);
-		System.out.println("The Fibbonacci of " + calc + " is: " +result);
+		long resultRec = computeRec(calc);
+		System.out.println("The Fibbonacci of " + calc + " is: " +result +" or recursively " + resultRec );
 		input.close();
 	}
 	public static long computeFibbonacci(long n) {
@@ -46,6 +47,24 @@ public class AppForTest {
 
 		}
 		return result;
+	}
+	public static long computeRec(long n) {
+		// if input is negative - throw exception
+		if (n < 0) {
+			throw new IllegalArgumentException();
+		}
+		// increase number of calls
+		
+		// base cases for 0 and 1
+		if (n == 0)
+			return 0;
+		else if (n == 1)
+			return 1;
+		// recursive case
+		else
+			// call the method with two previous values
+			return computeRec(n - 1) + computeRec(n - 2);
+
 	}
 
 }
